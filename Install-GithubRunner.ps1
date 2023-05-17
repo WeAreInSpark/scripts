6e0d9442f5ea6c248adafbeb05b0462ac057d95b
donheerschap <donheerschap@gmail.com>
2023-05-17 11:16:44 +0200

    Update Install-GithubRunner.ps1

--- Install-GithubRunner.ps1
+++ Install-GithubRunner.ps1
@@ -13,11 +13,18 @@ param(
   $labels
 )
 
+# Fail save for version input
+if ( $version[0] -ne "v" )
+{
+    $version = "v$version"
+}
+$versionUnprefixed = $version.Replace("v", "")
+
 # Create a folder under the drive root
 mkdir actions-runner; cd actions-runner
 
 # Download the latest runner package
-Invoke-WebRequest -Uri https://github.com/actions/runner/releases/download/$version/actions-runner-win-x64-$version.zip -OutFile actions-runner.zip
+Invoke-WebRequest -Uri https://github.com/actions/runner/releases/download/$version/actions-runner-win-x64-$versionUnprefixed.zip -OutFile actions-runner.zip
 
 # Extract the installer
 Add-Type -AssemblyName System.IO.Compression.FileSystem
