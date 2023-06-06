41dcae9c249cfecaeece6b3472311d0a9928dfd3
pvorselaars <perry.vorselaars@inspark.nl>
2023-06-06 09:31:44 +0200

    Enable agent deregister on termination notification (#8)

---
+++ Managed-Oxygen/Uninstall-ADOAgent.ps1
@@ -0,0 +1,19 @@
+param (
+    [System.Security.SecureString][Parameter(Mandatory=$true)]$pat,
+)
+
+$events = Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET -Uri "http://169.254.169.254/metadata/scheduledevents?api-version=2020-07-01" | Select-Object -ExpandProperty events
+$instance_name = Invoke-RestMethod -Headers @{"Metadata"="true"} -Method GET  -Uri "http://169.254.169.254/metadata/instance/compute?api-version=2021-02-01" | Select-Object -ExpandProperty Name
+
+foreach($event in $events){
+
+    if($event.EventType -eq "Terminate" -and $instance_name -in $event.Resources){
+        # VM is scheduled to shutdown. Uninstall runtime to deregister this node
+        c:\agent\config remove --unattended --auth pat --token (ConvertFrom-SecureString -AsPlainText $pat)
+
+        # Acknowledge event to expedite shutdown
+        $id = $event.EventId
+        Invoke-RestMethod -Headers @{"Metadata" = "true"} -Method POST -body "{""StartRequests"": [{""EventId"": ""$id""}]}" -Uri http://169.254.169.254/metadata/scheduledevents?api-version=2020-07-01
+    }
+
+}
