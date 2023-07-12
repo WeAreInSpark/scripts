d1488257ce5607d9921736b454595fc9c348247b
dylan-prins <8777848+Dylan-Prins@users.noreply.github.com>
2023-07-12 09:10:12 +0200

    fixing script

--- Managed-Oxygen/Install-Toolchain.ps1
+++ Managed-Oxygen/Install-Toolchain.ps1
@@ -7,6 +7,7 @@ Remove-Item .\AzureCLI.msi
 "- Installing Azure PowerShell"
 # Azure Powershell
 if (!(Get-Module -ListAvailable Az)) {
+    Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
     Install-Module -Name Az -Repository PSGallery -Force
 }
 
@@ -14,7 +15,10 @@ if (!(Get-Module -ListAvailable Az)) {
 # dotnet
 Invoke-WebRequest -Uri 'https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.ps1' -OutFile dotnet-install.ps1
 ./dotnet-install.ps1
+dotnet nuget add source https://api.nuget.org/v3/index.json -n nuget.org
 
 "- Installing DACPAC"
 # DACPAC
 dotnet tool install -g microsoft.sqlpackage
+
+-repo 'WeAreInSpark/Solution.ManagedOxygen' -token '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' -labels @('test')
