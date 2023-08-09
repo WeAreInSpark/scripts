db6c642b400498f940182b6c859c296478da0bf8
dylan-prins <8777848+Dylan-Prins@users.noreply.github.com>
2023-08-09 14:29:05 +0200

    fixing script

--- Managed-Oxygen/Install-Runtime.ps1
+++ Managed-Oxygen/Install-Runtime.ps1
@@ -1,5 +1,3 @@
-# [Diagnostics.CodeAnalysis.SuppressMessageAttribute("PSUseShouldProcessForStateChangingFunctions", Scope = "Function", Target = "New-RuntimeScheduledTask")]
-
 param(
     [Parameter(Mandatory)]
     [string]
@@ -25,41 +23,41 @@ function Get-Gateway([string] $path) {
         throw "Cannot download Integration Runtime installer to $path."
     }
 
-    Write-Output "Integration Runtime installer has been downloaded to $path."
+    "Integration Runtime installer has been downloaded to $path."
 }
 
 function Install-Gateway([string] $gwPath) {
 
-    Write-Output "Start Microsoft Integration Runtime installation"
+    "Start Microsoft Integration Runtime installation"
 
     $process = Start-Process "msiexec.exe" "/i $gwPath /quiet /passive" -Wait -PassThru
     if ($process.ExitCode -ne 0) {
         throw "Failed to install Microsoft Integration Runtime. msiexec exit code: $($process.ExitCode)"
     }
 
-    Write-Output "Succeed to install Microsoft Integration Runtime"
+    "Succeed to install Microsoft Integration Runtime"
 }
 
 function Register-Gateway([string] $key, [string] $port, [string] $cert) {
     $cmd = Get-CmdFilePath
 
     if (![string]::IsNullOrEmpty($port)) {
-        Write-Output "Start to enable remote access."
+        "Start to enable remote access."
         $process = Start-Process $cmd "-era $port $cert" -Wait -PassThru -NoNewWindow
         if ($process.ExitCode -ne 0) {
             throw "Failed to enable remote access. Exit code: $($process.ExitCode)"
         }
-        Write-Output "Succeed to enable remote access."
+        "Succeed to enable remote access."
     }
 
-    Write-Output "Start to register Microsoft Integration Runtime with key $key."
+    "Start to register Microsoft Integration Runtime with key $key."
     $process = Start-Process $cmd "-rn $key $node" -Wait -PassThru -NoNewWindow
 
     if ($process.ExitCode -ne 0) {
         throw "Failed to register Microsoft Integration Runtime. Exit code: $($process.ExitCode)"
     }
 
-    Write-Output "Succeed to register Microsoft Integration Runtime."
+    "Succeed to register Microsoft Integration Runtime."
 }
 
 function Get-CmdFilePath() {
@@ -68,24 +66,26 @@ function Get-CmdFilePath() {
         throw "Get-InstalledFilePath: Cannot find installed File Path"
     }
 
-    return (Split-Path -Parent $filePath) + "\dmgcmd.exe"
+    (Split-Path -Parent $filePath) + "\dmgcmd.exe"
 }
 
 function New-RuntimeScheduledTask() {
+    [CmdletBinding(SupportsShouldProcess)]
     param()
 
     $path = Join-Path $PSScriptRoot "Uninstall-Runtime.ps1"
 
     $params = @{
-        action = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "-NoProfile -File $path"
-        trigger = New-ScheduledTaskTrigger -Once -RepetitionInterval (New-TimeSpan -Minutes 1) -At (Get-Date)
-        user = "System"
-        taskname = "Watchdog"
+        action      = New-ScheduledTaskAction -Execute 'Powershell.exe' -Argument "-NoProfile -File $path"
+        trigger     = New-ScheduledTaskTrigger -Once -RepetitionInterval (New-TimeSpan -Minutes 1) -At (Get-Date)
+        user        = "System"
+        taskname    = "Watchdog"
         description = "Watch for VM termination events to deregister nodes from ADF runtime"
-
     }
 
-    Register-ScheduledTask @params
+    if ($PSCmdlet.ShouldProcess($user)) {
+        Register-ScheduledTask @params
+    }
 }
 
 If (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole(`
@@ -100,4 +100,3 @@ Get-Gateway $installerPath
 Install-Gateway $installerPath
 Register-Gateway $key 80
 New-RuntimeScheduledTask
-
