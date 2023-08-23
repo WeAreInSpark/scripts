b3950c5b29d4d815d0cb7c49fd238a593439cf8d
dylan-prins <8777848+Dylan-Prins@users.noreply.github.com>
2023-08-23 11:12:30 +0200

    fixing toolchain

--- Managed-Oxygen/Install-Toolchain.ps1
+++ Managed-Oxygen/Install-Toolchain.ps1
@@ -1,37 +1,39 @@
 "- Installing Azure CLI"
-# Azure CLI
+"  - Downloading file"
 Invoke-WebRequest -Uri https://aka.ms/installazurecliwindows -OutFile .\AzureCLI.msi
+"  - start installation"
 Start-Process msiexec.exe -Wait -ArgumentList '/I AzureCLI.msi /quiet'
-Remove-Item .\AzureCLI.msi
 
 "- Installing Azure PowerShell"
-# Azure Powershell
 if (!(Get-Module -ListAvailable Az)) {
     Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
     Install-Module -Name Az -Repository PSGallery -Force
 }
 
 "- Installing .NET"
-# dotnet
+"  - Downloading file"
 Invoke-WebRequest -Uri 'https://dotnet.microsoft.com/download/dotnet/scripts/v1/dotnet-install.ps1' -OutFile dotnet-install.ps1
+"  - start installation"
 ./dotnet-install.ps1
 dotnet nuget add source https://api.nuget.org/v3/index.json -n nuget.org
 
-"- Installing DACPAC"
-# DACPAC
-dotnet tool install -g microsoft.sqlpackage
+#"- Installing DACPAC"
+#dotnet tool install -g microsoft.sqlpackage
 
 "- Installing Powershell"
-# Powershell
-Invoke-WebRequest https://github.com/PowerShell/PowerShell/releases/download/v7.3.6/PowerShell-7.3.6-win-x64.msi -OutFile .\Powershell.msi -
-Start-Process msiexec.exe -Wait -ArgumentList '/I Powershell.msi /quiet' -Confirm:$false
+"  - Downloading file"
+Invoke-WebRequest https://github.com/PowerShell/PowerShell/releases/download/v7.3.6/PowerShell-7.3.6-win-x64.msi -OutFile .\Powershell.msi
+"  - start installation"
+Start-Process msiexec.exe -Wait -ArgumentList '/I Powershell.msi /quiet'
 
 "- Installing DACPAC Framework"
-# Dacpac Framework
+"  - Downloading file"
 Invoke-WebRequest https://aka.ms/dacfx-msi -OutFile .\DacpacFramework.msi
-Start-Process msiexec.exe -Wait -ArgumentList '/I DacpacFramework.msi /quiet' -Confirm:$false
+"  - start installation"
+Start-Process msiexec.exe -Wait -ArgumentList '/I DacpacFramework.msi /quiet'
 
 "- Installing bash"
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux -NoRestart
 wsl --install
 wsl --install -d Ubuntu
 
